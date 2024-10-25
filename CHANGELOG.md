14655940d6fc2db90418be7bf60e70aee28e0e81
Wes Murray <wescmx@gmail.com>
2024-10-25 17:15:33 +0200

    First commit

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## [1.0.0] - 2024-10-25
+
+### Added
+- Initial release of the ELB Monitoring module.
+- Support for monitoring Elastic Load Balancers (ELBs) with custom metric filters and alarms.
+- Configurable parameters for ELB names, event names, and CloudWatch log settings.
+- Automatic creation of CloudWatch metric filters based on specified ELBs and their respective events.
+- Alarms triggered based on defined thresholds for the specified metrics.
+- Detailed variable descriptions for easy customization and configuration.
---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "aws_cloudwatch_log_metric_filter" "elb_metric_filter" {
+  count          = length(var.elb_names)
+  log_group_name = var.cw_log_group_name
+  name           = "${var.elb_names[count.index]}-metric-filter"
+  pattern        = "{ ($.eventSource = elasticloadbalancing.amazonaws.com) && ($.requestParameters.loadBalancerName = \"${var.elb_names[count.index]}\") && ($.eventName = \"${join("\" || $.eventName = \"", var.elb_event_names)}\") }"
+
+  metric_transformation {
+    name      = "${var.elb_names[count.index]}-metric-filter"
+    namespace = var.cw_metric_filter_namespace
+    value     = var.cw_metric_filter_value
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "elb_metric_filter_alarm" {
+  count               = length(var.elb_names)
+  alarm_name          = "${var.elb_names[count.index]}-metric-filter-alarm"
+  comparison_operator = var.cw_metric_filter_alarm_comparison_operator
+  evaluation_periods  = var.cw_metric_filter_alarm_evaluation_periods
+  metric_name         = "${var.elb_names[count.index]}-metric-filter"
+  namespace           = var.cw_metric_filter_namespace
+  period              = var.cw_metric_filter_alarm_period
+  statistic           = var.cw_metric_filter_alarm_statistic
+  threshold           = var.cw_metric_filter_alarm_threshold
+  alarm_description   = "Alarm when ELB ${var.elb_names[count.index]} has >= 1 data points within 5 minutes."
+  alarm_actions       = var.cw_metric_filter_alarm_actions
+}
